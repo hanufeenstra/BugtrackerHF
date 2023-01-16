0c38eef1b8b66b4fd04bb6c90d5d86f0f6348057
hanufeenstra <Hanu167985423>
2023-01-16 14:29:51 +0200

    Added members to ProfileViewModel

--- src/Models/ViewModels/ProfileViewModel.cs
+++ src/Models/ViewModels/ProfileViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace BugtrackerHF.Models.ViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugtrackerHF.Models.ViewModels;
 
 public class ProfileViewModel
 {
-    
+    [Required]
+    public string? UserNickname { get; set; }
+    [Required]
+    public string? UserPicture { get; set; }
+    [Required]
+    [EmailAddress]
+    public string? UserEmail { get; set; }
 }
