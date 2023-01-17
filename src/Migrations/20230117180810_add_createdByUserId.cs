79fda8334a111755f81cd84202a7cee4df70dcf6
hanufeenstra <hanufeenstra@gmail.com>
2023-01-17 20:49:47 +0200

    Added migration

---
+++ src/Migrations/20230117180810_add_createdByUserId.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugtrackerHF.Migrations
+{
+    public partial class add_createdByUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IssueName",
+                table: "IssueModel",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ReportedByUserId",
+                table: "IssueModel",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReportedByUserId",
+                table: "IssueModel");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IssueName",
+                table: "IssueModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
