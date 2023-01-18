3c1c21580c318b02e9a7e90a212094c0af0c63d7
hanufeenstra <Hanu167985423>
2023-01-18 15:28:32 +0200

    Added check to see whether claim has already been transformed.

--- src/Services/ClaimsTransformation.cs
+++ src/Services/ClaimsTransformation.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
 using BugtrackerHF.DAL.UnitOfWork;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -16,12 +17,17 @@ public class ClaimsTransformation : IClaimsTransformation
         _logger = logger;
     }
 
+    [SuppressMessage("ReSharper.DPA", "DPA0009: High execution time of DB command", MessageId = "time: 2849ms")]
     public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
         // Clone current identity
         var clone = principal.Clone();
         var newIdentity = clone.Identity as ClaimsIdentity;
-
+        
+        // Checks whether the claim has previously been modified
+        if (principal.Claims.FirstOrDefault(c => c.ValueType == "Transformed")?.Value == "True")
+            return clone;
+        
         // Support AD and local accounts
         var authZeroId = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
         
@@ -55,10 +61,14 @@ public class ClaimsTransformation : IClaimsTransformation
 
         // Add userNickname to cloned identity
         var userNickname = new Claim("UserNickname", user.UserNickname!);
+        
+        // Add transformedClaim to cloned identity
+        var isTransformed = new Claim("Transformed", "True");
 
         newIdentity.AddClaim(userId);
         newIdentity.AddClaim(userPicture);
         newIdentity.AddClaim(userNickname);
+        newIdentity.AddClaim(isTransformed);
 
         _logger.LogInformation("Transformed claim. Added userId: {0}, userPicture: {1}, userNickname: {2}", userId, userPicture, userNickname);
 
