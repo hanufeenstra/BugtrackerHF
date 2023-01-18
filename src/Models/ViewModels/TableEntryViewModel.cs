d02fe560e6c0f8eb14b75c684dc4bf51c4f2644a
hanufeenstra <Hanu167985423>
2023-01-18 15:36:22 +0200

    Created a view specif model vir the MyIssues table

---
+++ src/Models/ViewModels/TableEntryViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BugtrackerHF.Models.ViewModels;
+
+public class TableEntryViewModel
+{
+    public int RouteValue { get; set; }
+    public Status CurrentStatus { get; set; }
+    public string CreatorPicture { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string ProjectName { get; set; }
+    public DateTime LastModified { get; set; }
+    public Severity CurrentSeverity { get; set; }
+}
